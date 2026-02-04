3dc103e4845420866e59411dbcceca365e304cd1
oxteklabala <oxtekdj@gmail.com>
2026-02-04 14:17:10 -0500

    Add config.py with configuration settings

---
+++ config.py
@@ -0,0 +1,18 @@
+# Configuration settings for ALETEONETWORK
+
+HASHTAGS = ['#example1', '#example2', '#example3']
+
+DARK_DESCRIPTORS = ['dark1', 'dark2', 'dark3']
+
+HEAVY_DESCRIPTORS = ['heavy1', 'heavy2', 'heavy3']
+
+EXPERIMENTAL_DESCRIPTORS = ['experimental1', 'experimental2']
+
+RARE_COMBINATIONS = [('combo1', 'combo2'), ('combo3', 'combo4')]
+
+CONTENT_PILLARS = ['pillar1', 'pillar2', 'pillar3']
+
+BEST_POSTING_TIMES = ['09:00', '12:00', '18:00']
+
+# Database file settings
+DATABASE_FILE = 'path/to/database.db'
